ff22d66b0383efaf887d141b0fd7da2a02fb8bf1
otihso <46724524+otihso@users.noreply.github.com>
2019-01-15 14:09:01 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+// HelloPrinter.java
+// Lab #1
+// Sarah Otih
+// 01/15/2019
+// CMSC-255-002
+
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Sarah Otih");
+		System.out.println("HelloPrinter.java");
+		System.out.println("Lab #1"); 
+		System.out.println("01/15/2019"); 
+		System.out.println("CMSC-255-002"); 
+		System.out.println(); 
+	}
+	public static void main(String [] args) {
+		printHeading(); 
+		System.out.println("Hello"); 
+	}
+}
